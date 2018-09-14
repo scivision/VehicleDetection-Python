ad9cd31ce5afc51c46585404af694774fe480094
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2018-09-14 18:34:49 -0400

    make plot

--- blob.py
+++ blob.py
@@ -21,7 +21,7 @@ def main():
     fn = Path(p.infn).expanduser()
 
     with h5py.File(fn, 'r') as f:
-        mot = f['motion'][:].astype(np.uint8)
+        mot = np.rot90(f['motion'][1025:,...].astype(np.uint8), axes=(1,2))
 
     bmot = mot > 15
 
@@ -31,6 +31,11 @@ def main():
         ax = figure(1).gca()
     else:
         B = setupblob(P)
+        bmot = bmot.astype(np.uint8) * 255
+        fourcc = cv2.VideoWriter_fourcc(*'FFV1')
+        hv = cv2.VideoWriter(str(fn.parent/'cv2final.avi'), fourcc,
+                             fps=10,
+                             frameSize=(30, 41))
 
     for i, m in enumerate(bmot):
 
@@ -40,15 +45,17 @@ def main():
 
             sblob(m, ax)
         else:
-            final, nkey, kpsize = cv2blob(mot[i], m, B)
+            final, nkey, kpsize = cv2blob(mot[i]*2, m, B)
             cv2.imshow('result', final)
+            hv.write(final)
             cv2.waitKey(1)
+
+    if ax is None:
+        hv.release()
+        cv2.destroyAllWindows()
 
 
 def cv2blob(img: np.ndarray, motion: np.ndarray, B):
-    if motion.dtype == np.bool:
-        motion = motion.astype(np.uint8) * 255
-
     keypoints = B.detect(motion)
     nkey = len(keypoints)
     kpsize = np.asarray([k.size for k in keypoints])
@@ -57,9 +64,10 @@ def cv2blob(img: np.ndarray, motion: np.ndarray, B):
     final = cv2.drawKeypoints(img, keypoints, outImage=final,
                               flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 # %% plot count of blobs
-    cv2.putText(final, text=str(nkey), org=(int(img.shape[1]*.9), 25),
-                fontFace=cv2.FONT_HERSHEY_PLAIN, fontScale=2,
-                color=(0, 255, 0), thickness=2)
+    cv2.putText(final, text=str(nkey),
+                org=(int(img.shape[1]*.7), int(img.shape[0]*.3)),
+                fontFace=cv2.FONT_HERSHEY_PLAIN, fontScale=0.8,
+                color=(0, 255, 0), thickness=1)
 
     return final, nkey, kpsize
 
