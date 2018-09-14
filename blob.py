9b9a7ba21e25291f751f65c3566d853dc24a535a
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2018-09-14 17:46:19 -0400

    add cv2

--- blob.py
+++ blob.py
@@ -5,40 +5,102 @@ from pathlib import Path
 import numpy as np
 import skimage.feature as skif
 from matplotlib.pyplot import figure, draw, pause
+import cv2
+
+P = {'mindist': 3,
+     'minarea': 10,
+     'maxarea': 1000}
+
 
 def main():
     p = ArgumentParser()
     p.add_argument('infn', help='HDF5 motion file to analyze')
+    p.add_argument('-cv2', help='use openCV', action='store_true')
     p = p.parse_args()
-    
+
     fn = Path(p.infn).expanduser()
-    
+
     with h5py.File(fn, 'r') as f:
         mot = f['motion'][:].astype(np.uint8)
-        
-    bmot = mot > 15
-        
-    ax = figure(1).gca()
-    for i,m in enumerate(bmot):
-        ax.cla()
-        ax.imshow(bmot[i], origin='bottom')
-        
-        blobs = skif.blob_doh(m, min_sigma=5, max_sigma=10000, num_sigma=20, overlap=0.1)
-        # blobs = skif.blob_dog(m)
-        #blobs = skif.blob_log(m, min_sigma=10, max_sigma=300, num_sigma=10, threshold=.1, overlap=1)
-        
-        good = (blobs[:,0]>5) & (blobs[:,1]>5) & (blobs[:,0] < mot.shape[0]-5) & (blobs[:,1] < mot.shape[1]-5)
-        blobs = blobs[good, ...]
-        for b in blobs:
-            ax.scatter(b[1], b[0], b[2]*100)
-            
-        #if blobs.size > 0:
-        #    print(f'frame {i}, {blobs.shape[0]} blobs, max sigma {blobs[:,2].max()}')
 
-        
-        draw()
-        pause(0.05)
-        
+    bmot = mot > 15
+
+    B = None
+    ax = None
+    if not p.cv2:
+        ax = figure(1).gca()
+    else:
+        B = setupblob(P)
+
+    for i, m in enumerate(bmot):
+
+        if ax is not None:
+            ax.cla()
+            ax.imshow(bmot[i], origin='bottom')
+
+            sblob(m, ax)
+        else:
+            final, nkey, kpsize = cv2blob(mot[i], m, B)
+            cv2.imshow('result', final)
+            cv2.waitKey(1)
+
+
+def cv2blob(img: np.ndarray, motion: np.ndarray, B):
+    if motion.dtype == np.bool:
+        motion = motion.astype(np.uint8) * 255
+
+    keypoints = B.detect(motion)
+    nkey = len(keypoints)
+    kpsize = np.asarray([k.size for k in keypoints])
+    final = img.copy()  # is the .copy necessary?
+
+    final = cv2.drawKeypoints(img, keypoints, outImage=final,
+                              flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+# %% plot count of blobs
+    cv2.putText(final, text=str(nkey), org=(int(img.shape[1]*.9), 25),
+                fontFace=cv2.FONT_HERSHEY_PLAIN, fontScale=2,
+                color=(0, 255, 0), thickness=2)
+
+    return final, nkey, kpsize
+
+
+def setupblob(param: dict):
+    """
+    setup connected components "blob detection"
+    """
+    B = cv2.SimpleBlobDetector_Params()
+    B.filterByArea = True
+    B.filterByColor = False
+    B.filterByCircularity = False
+    B.filterByInertia = False
+    B.filterByConvexity = False
+
+    B.minDistBetweenBlobs = param['mindist']
+    B.minArea = param['minarea']
+    B.maxArea = param['maxarea']
+
+    # B.minThreshold = 1  # we have already made a binary image
+
+    return cv2.SimpleBlobDetector_create(B)
+
+
+def sblob(mot, ax):
+
+    blobs = skif.blob_doh(mot, min_sigma=5, max_sigma=10000, num_sigma=20, overlap=0.1)
+    # blobs = skif.blob_dog(m)
+    # blobs = skif.blob_log(m, min_sigma=10, max_sigma=300, num_sigma=10, threshold=.1, overlap=1)
+
+    good = (blobs[:, 0] > 5) & (blobs[:, 1] > 5) & (blobs[:, 0] < mot.shape[0]-5) & (blobs[:, 1] < mot.shape[1]-5)
+    blobs = blobs[good, ...]
+    for b in blobs:
+        ax.scatter(b[1], b[0], b[2]*100)
+
+    # if blobs.size > 0:
+    #    print(f'frame {i}, {blobs.shape[0]} blobs, max sigma {blobs[:,2].max()}')
+
+    draw()
+    pause(0.05)
+
 
 if __name__ == '__main__':
     try:
